d634f0324f92cf5fa951982402f320db75f0a8ba
Promochi0141 <130904564+Promochi0141@users.noreply.github.com>
2024-11-04 19:45:38 +0900

    allow CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // 全てのパスに対して適用
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: NEXT_PUBLIC_BACKEND_URL, // 許可するURLを指定
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,PUT,DELETE,OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-Requested-With, Content-Type, Authorization",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
