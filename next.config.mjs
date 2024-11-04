2444bda899dbfc6477f3e53ffa06634c045a4bb7
Promochi0141 <130904564+Promochi0141@users.noreply.github.com>
2024-11-04 19:52:34 +0900

    再変更

--- next.config.mjs
+++ next.config.mjs
@@ -5,22 +5,10 @@ const nextConfig = {
             {
                 source: "/(.*)", // 全てのパスに対して適用
                 headers: [
-                    {
-                        "key": "Access-Control-Allow-Credentials",
-                        "value": "true"
-                    },
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: "http://shift-api-2024.nodaridaisai.com", // 許可するURLを指定
-                    },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,POST,PUT,DELETE,OPTIONS",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "X-Requested-With, Content-Type, Authorization",
-                    },
+                    { "key": "Access-Control-Allow-Credentials", "value": "true" },
+                    { "key": "Access-Control-Allow-Origin", "value": "*" },
+                    { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
                 ],
             },
         ];
