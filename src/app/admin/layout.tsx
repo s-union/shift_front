022248723f9f37175bf4ebffb56aacc80469aad7
Promochi0141 <130904564+Promochi0141@users.noreply.github.com>
2024-11-04 19:00:43 +0900

    modalエラー解消

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Modal from 'react-modal';
-const modalStyle = {
+const modalStyle: ReactModal.Styles = {
     overlay: {
         position: "fixed",
         top: 0,
